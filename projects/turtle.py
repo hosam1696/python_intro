fea325204bc05d8a8745e26007c59383dcdd79ae
hosamsam <hosamelnabawy1996@gmail.com>
2018-03-30 22:45:19 +0200

    fix misloading wrong turtle module by changing the directories abd file with name `turtle`

--- projects/turtle.py
+++
@@ -1,5 +0,0 @@
-import turtle
-
-brad = turtle.Turtle()
-
-brad.showturtle()
---
+++ projects/turtlepython.py
@@ -0,0 +1,47 @@
+import turtle
+
+
+class Draw_turtle():
+
+    def __init__(self):
+        self.window = turtle.Screen()
+        self.hosam = turtle.Turtle()
+        self.init_window()
+        self.render()
+
+    def init_window(self):
+        self.window.screensize(canvwidth=200, canvheight=200)
+        self.window.bgcolor('#333333')
+        self.hosam.color('#eeeeee')
+        self.hosam.shape('turtle')
+        self.hosam.color('yellow')
+        self.hosam.fillcolor('white')
+        self.hosam.speed(10)
+
+    def render(self):
+        self.loop_in_circle(self.draw_rectangle)
+        """self.hosam.begin_fill()
+        self.hosam.end_fill()"""
+        self.window.exitonclick()
+
+    @staticmethod
+    def draw_square(turtle):
+        for i in range(0, 4):
+            turtle.fd(100)
+            turtle.right(90)
+
+    def draw_rectangle(self, turtle):
+        for i in range(0, 2):
+            turtle.fd(100)
+            turtle.right(120)
+        turtle.fd(100)
+
+
+    def loop_in_circle(self, cb):
+        for i in range(0, 360, 10):
+            self.hosam.right(i+120)
+            cb(self.hosam)
+            self.hosam.left(i)
+
+
+drawturtle = Draw_turtle()
